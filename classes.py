458eddc50841f8f1a8f6055b2acd826bc449a636
Andrew DaRe <andrew@duh.re>
2018-06-08 00:57:44 -0700

    Added Deploy class to parse deploy.json

--- classes.py
+++ classes.py
@@ -3,4 +3,8 @@ import json
 class Deploy():
 
     def __init__(self, file):
-        # self.branch = 
+        # self.payload = self.parse_json(file)
+        self.test = "test"
+
+    def parse_json(self, file):
+        return(json.loads(file))
