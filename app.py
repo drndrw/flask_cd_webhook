2b3521a8503543b295aa0ddc0f96a97ef4cbe8ee
Andrew DaRe <andrew@duh.re>
2018-06-07 23:12:39 -0700

    Check if 'branch' and 'token' keys exist

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/', methods = ['POST'])
 def deploy():
+    data = request.get_json()
+    # Check for branch and token keys
+    if data.get('branch') and data.get('token'):
+        print('Token and branch keys exist')
     return jsonify({'route':'deploy'})
 
 if __name__ == '__main__':
