9ba0212a158160ac7ccf27c0a609b8e7b961a944
Andrew DaRe <andrew@duh.re>
2018-06-10 13:16:47 -0700

    Iterate through different branch tokens

--- app.py
+++ app.py
@@ -14,13 +14,19 @@ with open('deploy.json') as f:
 @app.route('/', methods = ['POST'])
 def index():
     data = request.get_json()
-    print(app.deploy_json.payload)
+    # print(app.deploy_json.payload)
     # Check for branch and token keys
     if data.get('repository') and data.get('branch') and data.get('token'):
         # Check of respository exists in deploy.json
-        print('Repository, token and branch keys exist')
+        # print('Repository, token and branch keys exist')
+        # Check if repository name from request exists in deploy.json
         if app.deploy_json.payload.get(data.get('repository')):
             print('repository exists')
+            # Check if branch exists inside of repository
+            if app.deploy_json.payload[data.get('repository')].get(data.get('branch')):
+                print('branch exists inside of repo')
+                for token in app.deploy_json.payload[data.get('repository')][data.get('branch')]:
+                    print(token)
     return jsonify({'route':'deploy'})
 
 if __name__ == '__main__':
